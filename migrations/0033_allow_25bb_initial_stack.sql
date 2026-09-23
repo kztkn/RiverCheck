ae125df79d305e09956b2c8bb1e58a9529588433
kztkn <91022338+kztkn@users.noreply.github.com>
2026-09-24 05:50:02 +0900

    chore: keep existing stack options unchanged

--- migrations/0033_allow_25bb_initial_stack.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE games
-DROP CONSTRAINT games_initial_stack_bb_supported;
-
-ALTER TABLE games
-ADD CONSTRAINT games_initial_stack_bb_supported CHECK (
-  initial_stack_bb IN (25, 50, 100)
-);
